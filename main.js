66638478d4218cd20e400482f9f8232748b0c8c6
Ramani Shankar <ramshankarjan1222@gmail.com>
2022-09-07 17:17:16 +0530

    Task Completed

---
+++ main.js
@@ -0,0 +1,51 @@
+//JS Dataypes
+
+//***Number***
+var a=5;
+
+//**String ***
+ var n="Ramani Shankar";
+ 
+//***Boolean 
+  var x=10;
+  var y=20;
+  if(x==y){
+    console.log("true");
+  }
+  else{
+    console.log("false");
+  }
+
+//***Undefined 
+
+  var a;
+  console.log(a);
+  
+//**Null 
+   var a=null;
+   console.log(a);
+
+   //***Nan 
+var num1=10;
+var num2="Ramani";
+
+console.log(num1-num2);
+//**Array 
+var fruits=["Banana","Apple","Orange","Pineapple","Pomegranate"];
+console.log(fruits);
+
+//**Object */
+ var number={
+    n1:10,
+    n2:20,
+    n3:30
+}
+console.log(number);
+
+//*BigInt
+var num=57498456n;
+typeof(num);
+console.log(num);
+var num1=BigInt(982371562323);
+typeof(num1);
+
